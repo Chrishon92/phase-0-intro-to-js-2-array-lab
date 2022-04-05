1df79ed0518bad0cb056e5f7196909c59bf0231e
ChrishonCrum <chrishonkcrum@yahoo.com>
2022-04-05 08:44:08 -0500

    commit tree 9605cf67ecca2b6817aed2d3a67f6d2f32ff2416

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function appendCat(name) {
+    const allCats = [...cats, name]
+    return allCats
+}
+
+function prependCat(name) {
+    const allCats = [name, ...cats]
+    return allCats
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+} 1
